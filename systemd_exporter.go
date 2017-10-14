81fdb6554707939f388d5b1d666346cd1140e7d3
Arian van Putten <aeroboy94@gmail.com>
2017-10-14 11:20:45 +0200

    Export a single metric for services as a start

--- systemd_exporter.go
+++ systemd_exporter.go
@@ -1,38 +1,91 @@
 package main
 
 import (
+	"flag"
 	"github.com/coreos/go-systemd/dbus"
-        "fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"strings"
+	"net/http"
+)
+
+type metric struct {
+	desc      *prometheus.Desc
+	valueType prometheus.ValueType
+}
+type metricsMap map[string]metric
+
+type unitMetricsMap map[string]metricsMap
+
+type systemdCollector struct {
+	conn    *dbus.Conn
+	metrics unitMetricsMap
+}
+
+func (c *systemdCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, v := range c.metrics {
+		for _, v := range v {
+			ch <- v.desc
+		}
+	}
+}
+
+func (c *systemdCollector) Collect(ch chan<- prometheus.Metric) {
+	unitStatuses, err := c.conn.ListUnits()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, unitStatus := range unitStatuses {
+		splitted := strings.Split(unitStatus.Name, ".")
+		unitType := strings.Title(splitted[1])
+		for propName, metric := range c.metrics[unitType] {
+			prop, err := c.conn.GetUnitTypeProperty(unitStatus.Name, unitType, propName)
+			if err == nil {
+				value, ok := prop.Value.Value().(uint64)
+				if ok {
+					if value != ^uint64(0) {
+						ch <- prometheus.MustNewConstMetric(metric.desc, metric.valueType, float64(value), unitStatus.Name)
+					}
+				}
+			} else {
+				log.Print(err)
+			}
+		}
+	}
+}
+
+func newSystemdCollector(conn *dbus.Conn) *systemdCollector {
+	return &systemdCollector{
+		conn: conn,
+		metrics: unitMetricsMap{
+			"Service": metricsMap{
+				"CPUUsageNSec": metric{
+					desc:      prometheus.NewDesc("systemd_service_cpu_usage_nanoseconds_total", "Total CPU seconds of a unit", []string{"unit"}, nil),
+					valueType: prometheus.CounterValue,
+				},
+				"MemoryCurrent": metric{
+					desc:      prometheus.NewDesc("systemd_service_memory_current_bytes", "Amount of bytes", []string{"unit"}, nil),
+					valueType: prometheus.GaugeValue,
+				},
+			},
+		},
+	}
+}
+
+var (
+	addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 )
 
 func main() {
-  conn, err := dbus.New()
-  if err != nil {
-  }
-  defer conn.Close()
-
-  unitStatuses, err := conn.ListUnits()
-  if err != nil {
-  }
-
-  for _, unitStatus := range unitStatuses {
-    fmt.Printf("%s\n", unitStatus.Name)
-
-    properties, err := conn.GetUnitProperties(unitStatus.Name)
-
-    if err != nil {
-    }
-
-    for name, value := range properties {
-      value, ok := value.(uint64)
-
-      if ok {
-        fmt.Printf("%+v=%+v\n", name, value)
-      }
-    }
-
-
-  }
-
-
+	flag.Parse()
+	conn, err := dbus.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	systemdCollector := newSystemdCollector(conn)
+	prometheus.MustRegister(systemdCollector)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
